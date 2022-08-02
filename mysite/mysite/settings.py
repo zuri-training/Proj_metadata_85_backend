095913d65846dd71e4acda4c7b3c694e20ddca55
nddic <cidnoir81@gmail.com>
2022-08-02 18:58:18 +0100

    made changes in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ['SECRET KEY']
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
