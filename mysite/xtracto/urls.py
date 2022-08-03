82501fa18ed2148a8ba566a272d401f6af062688
Yino IT <97189078+Yinotherhino@users.noreply.github.com>
2022-08-03 10:57:21 +0100

    Add files via upload

--- mysite/xtracto/urls.py
+++ mysite/xtracto/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path, re_path
 from . import views
 
 app_name = "xtracto"
 
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register",)
+    path("", views.index, name="home"),
+    path("register/", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("registerfrontend/", views.register, name="registerfrontend"),
+    path("loginfrontend/", views.login, name="loginfrontend"),
+    # path("register/", views.register, name=""),
 ]
--- mysite/xtracto/views.py
+++ mysite/xtracto/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
+def index(request):
+    return render(request, 'xtracto/home.html')
+
+def about(request):
+    return render(request, 'xtracto/About-us.html')
+
+def contact(request):
+    return render(request, 'xtracto/contact.html')
+
+def register(request):
+    return render(request, 'xtracto/registerfrontend.html')
+
+def login(request):
+    return render(request, 'xtracto/loginfrontend.html')
+
+
+
+
+
+
 
 
 def register_request(request):
@@ -15,4 +37,29 @@ def register_request(request):
         messages.error(
             request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
-    return render(request=request, template_name="main/register.html", context={"register_form": form})
+    return render(request=request, template_name="xtracto/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="xtracto/login.html", context={"login_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("main:homepage")
