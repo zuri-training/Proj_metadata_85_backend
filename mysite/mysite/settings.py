0928e3d3b2445260eb5e14041c78e9658e1fe0f7
Timi Adenuga <timswaggs1@outlook.com>
2022-08-04 23:43:38 +0100

    added database to settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xtracto',
+        'USER': 'team_85',
+        'PASSWORD': 'XtrTeam85acto',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
     
 }
