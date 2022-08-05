f046c340e9a05bccfb2c05241b2818989a3b97de
Yinotherhino <97189078+Yinotherhino@users.noreply.github.com>
2022-08-05 17:22:37 +0100

    Register process

--- mysite/xtracto/urls.py
+++ mysite/xtracto/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from . import views
-from .views import index, register_request, login_request, login_request, about, contact, register, login
+from .views import *
 
 app_name = "xtracto"
 
@@ -9,7 +9,7 @@ app_name = "xtracto"
 urlpatterns = [
     path("", views.index, name="home"),
     path("register/", views.register, name="register"),
-    path("login", views.login_request, name="login"),
+    path("login", views.login, name="login"),
     path("logout", views.logout_request, name="logout"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
@@ -18,5 +18,4 @@ urlpatterns = [
     path("loginfrontend/", views.login, name="loginfrontend"),
     path("collections/", views.collections, name="collections"),
     path("features/", views.features, name="features"),
-    # path("register/", views.register, name=""),
 ]
--- mysite/xtracto/views.py
+++ mysite/xtracto/views.py
@@ -22,25 +22,34 @@ def register(request):
             post.email= request.POST.get('email')
             post.password= request.POST.get('password')
             post.save()
-            mydictionary= {}
-            mydictionary['SuccessMsg'] = 'Form Submitted: You can now login'
-            return render(request, 'xtracto/login.html', context=mydictionary)
-    #     else:
-            
-    #         mydictionary = {
-    #             "form" : form
-    #         }
-    #         return render(request,'xtracto/register.html', context=mydictionary)
+            # mydictionary= {}
+            # mydictionary['SuccessMsg'] = 'Form Submitted: You can now login'
+            messages.success(request, 'You nhave been succefully registered')
+            return render(request, 'xtracto/login.html', )
+        else:
+            return render(request,'xtracto/register.html')  
 
     else:        
         return render(request,'xtracto/register.html')
 
 
-
-
-
 def login(request):
+    if request.method == 'POST':
+        if request.POST.get('email') and request.POST.get('password'):
+            email =request.POST['email']
+            password=request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                email= user.email
+                return render(request, 'xtracto/dashboard.html', {'email': email})  
+            else:
+                messages.error(request, 'Invalid details')
+                return redirect('xtracto/home')
     return render(request, 'xtracto/loginfrontend.html')
+
+
+
 
 
 
@@ -56,37 +65,10 @@ def features(request):
 
 
 
-def register_request(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect("main:homepage")
-        messages.error(
-            request, "Unsuccessful registration. Invalid information.")
-    form = NewUserForm()
-    return render(request=request, template_name="xtracto/register.html", context={"register_form": form})
 
 
-def login_request(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
-                return redirect("xtracto:home")
-            else:
-                messages.error(request, "Invalid username or password.")
-        else:
-            messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request=request, template_name="xtracto/login.html", context={"login_form": form})
+
+
 
 
 def logout_request(request):
