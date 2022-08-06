92f15b4a6048df527b8ab3b5f529f45b83c2bd37
Timilehin Adenuga <timswaggs1@outlook.com>
2022-08-06 20:19:27 +0100

    pushing to heroku

--- src/xtracto/settings.py
+++ src/xtracto/settings.py
@@ -139,14 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   BASE_DIR / "staticfiles"
 STATIC_URL = 'apps/pages/static/'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
