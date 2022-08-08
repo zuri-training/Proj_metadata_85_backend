dc97dabe42efdce0cfe41da0ebd78898902cd770
Yinotherhino <97189078+Yinotherhino@users.noreply.github.com>
2022-08-08 09:49:54 +0100

    proper links

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -8,13 +8,13 @@ app_name = "xtracto"
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("registerfrontend/", views.register, name="registerfrontend"),
+    # path("registerfrontend/", views.register, name="registerfrontend"),
     path("collections/", views.collections, name="collections"),
     path("features/", views.features, name="features"),
     path("faqs/", views.faqs, name="faqs"),
