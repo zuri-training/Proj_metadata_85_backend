9238510326fa49dc67d290bb35e97fb9e6da5f62
Yinotherhino <97189078+Yinotherhino@users.noreply.github.com>
2022-08-08 09:49:19 +0100

    proper links

--- src/pages/static/js/faqs.js
+++ src/pages/static/js/faqs.js
@@ -1,7 +1,7 @@
-for (let i=0; i=5; i++){
+for (let i=0; i<6; i++){
     $(".faq-drop-"+ i).click(function(){
 
-        $('.faq-drop-'+i).next().toggle(500);
+        $('.faq-drop-'+ i).next().toggle(500);
     
     });
 }
