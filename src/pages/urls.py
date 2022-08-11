22c4e4321c7134061e1061153025ecb0eab8cf55
nddic <cidnoir81@gmail.com>
2022-08-11 15:02:12 +0100

    update password reset

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import *
+from .views import PasswordsChangeView
 
 app_name = "xtracto"
 
@@ -19,16 +19,9 @@ urlpatterns = [
     path("features/", views.features, name="features"),
     path("docs/", views.docs, name="docs"),
     path("faqs/", views.faqs, name="faqs"),
-
-    # password reset urls
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='xtracto/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name="xtracto/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='xtracto/password_reset_complete.html'), name='password_reset_complete'),
-    path("password_reset", views.password_reset_request, name="password_reset"),
-
+    path("password/", PasswordsChangeView.as_view(
+        template_name="xtracto/password_reset_confirm.html")),
+        
     # -----metadata urls-----#
 
     path("download", views.download_csv_data, name="download"),
@@ -39,3 +32,12 @@ urlpatterns = [
     path("metadata_view/<int:pk>", views.metadata_view, name="metadata_view"),
     path("records/<int:pk>", views.records.as_view(), name="records"),
 ]
+
+# password reset urls
+# path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+#     template_name='xtracto/password_reset_done.html'), name='password_reset_done'),
+# path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+#     template_name="xtracto/password_reset_confirm.html"), name='password_reset_confirm'),
+# path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+#     template_name='xtracto/password_reset_complete.html'), name='password_reset_complete'),
+# path("password_reset", views.password_reset_request, name="password_reset"),
