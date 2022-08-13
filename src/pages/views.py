49cdc3dc11387f894ffc6ff570a2dbada5678e24
Yinotherhino <97189078+Yinotherhino@users.noreply.github.com>
2022-08-13 13:38:24 +0100

    Update views.py

--- src/pages/views.py
+++ src/pages/views.py
@@ -123,34 +123,34 @@ def verify(request):
 #     ['yinorhino@gmail.com'],
 #     fail_silently=False,
 # )
-    sender_email = "my@gmail.com"
-    receiver_email = "your@gmail.com"
-    message = """\
-    Subject: Hi there
+    # sender_email = "my@gmail.com"
+    # receiver_email = "your@gmail.com"
+    # message = """\
+    # Subject: Hi there
 
-    This message is sent from Python."""
-    smtp_server = "smtp.mail.yahoo.com"
-    port = 587  # For starttls
-    sender_email = "muhammedbayero@yahoo.com"
-    password = 'enter_password'
+    # This message is sent from Python."""
+    # smtp_server = "smtp.mail.yahoo.com"
+    # port = 587  # For starttls
+    # sender_email = "muhammedbayero@yahoo.com"
+    # password = 'enter_password'
 
-    # Create a secure SSL context
-    context = ssl.create_default_context()
+    # # Create a secure SSL context
+    # context = ssl.create_default_context()
 
-    # Try to log in to server and send email
-    try:
-        server = smtplib.SMTP(smtp_server,port)
-        server.ehlo() # Can be omitted
-        server.starttls(context=context) # Secure the connection
-        server.ehlo() # Can be omitted
-        server.login(sender_email, password)
-        # TODO: Send email here
-        server.sendmail(sender_email, receiver_email, message)
-    except Exception as e:
-        # Print any error messages to stdout
-        print(e)
-    finally:
-        server.quit() 
+    # # Try to log in to server and send email
+    # try:
+    #     server = smtplib.SMTP(smtp_server,port)
+    #     server.ehlo() # Can be omitted
+    #     server.starttls(context=context) # Secure the connection
+    #     server.ehlo() # Can be omitted
+    #     server.login(sender_email, password)
+    #     # TODO: Send email here
+    #     server.sendmail(sender_email, receiver_email, message)
+    # except Exception as e:
+    #     # Print any error messages to stdout
+    #     print(e)
+    # finally:
+    #     server.quit() 
     return render(request, "xtracto/verify.html")
 
 def register_request(request):
