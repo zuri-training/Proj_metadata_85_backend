f61bdf766e96264ef4e2f42b40f8bb85d0a4960a
Yinotherhino <97189078+Yinotherhino@users.noreply.github.com>
2022-08-21 23:40:38 +0100

    to postgres

--- src/xtracto/settings.py
+++ src/xtracto/settings.py
@@ -93,11 +93,31 @@ WSGI_APPLICATION = "xtracto.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'metadata',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'metadata',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
